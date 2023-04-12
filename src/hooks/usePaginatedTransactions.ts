5700b55406e338dd8bf4887ee35d968e29afa215
ThapeloMasasa <tmasasa@nd.edu>
2023-04-11 23:11:37 -0400

    corrected Ramp version

---
+++ src/hooks/usePaginatedTransactions.ts
@@ -0,0 +1,37 @@
+import { useCallback, useState } from "react"
+import { PaginatedRequestParams, PaginatedResponse, Transaction } from "../utils/types"
+import { PaginatedTransactionsResult } from "./types"
+import { useCustomFetch } from "./useCustomFetch"
+
+export function usePaginatedTransactions(): PaginatedTransactionsResult {
+  const { fetchWithCache, loading } = useCustomFetch()
+  const [paginatedTransactions, setPaginatedTransactions] = useState<PaginatedResponse<
+    Transaction[]
+  > | null>(null)
+
+  const fetchAll = useCallback(async () => {
+    const response = await fetchWithCache<PaginatedResponse<Transaction[]>, PaginatedRequestParams>(
+      "paginatedTransactions",
+      {
+        page: paginatedTransactions === null ? 0 : paginatedTransactions.nextPage,
+      }
+    )
+
+    setPaginatedTransactions((previousResponse) => {
+      
+      if (response === null || previousResponse === null) {
+        return response
+      }
+
+      // 👉 Bug 4 fixed
+      const dataToSend = [...previousResponse.data, ...response.data]
+      return { data: dataToSend, nextPage: response.nextPage }
+    })
+  }, [fetchWithCache, paginatedTransactions])
+
+  const invalidateData = useCallback(() => {
+    setPaginatedTransactions(null)
+  }, [])
+
+  return { data: paginatedTransactions, loading, fetchAll, invalidateData }
+}
